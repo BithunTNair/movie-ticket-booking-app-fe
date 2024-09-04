d87690b3c5ea7f1afe9c7d60e984a1704c87ed07
BithunT-Nair <react666cr@gmail.com>
2024-09-04 15:45:33 +0530

    pay success

--- src/components/Theatre/TheatreSeats.jsx
+++ src/components/Theatre/TheatreSeats.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import AxiosInstance from '../../Config/ApiCall';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { successToast } from '../../Plugins/Toast';
 
 function TheatreSeats() {
     const [seats, setSeats] = useState([]);
     const { showsid, id } = useParams();
     const [selectedSeats, setSelectedSeats] = useState([]);
-    // const [movieId, setMovieId] = useState('');
+    const [movieId, setMovieId] = useState('');
+    const {user}= useSelector(store=>store.user)
 
 
 
@@ -15,7 +18,7 @@ function TheatreSeats() {
 
     useEffect(() => {
         getSeats();
-        // getMovieforThisShow();
+        getMovieforThisShow();
     }, [showsid]);
 
     const getSeats = async () => {
@@ -34,20 +37,20 @@ function TheatreSeats() {
         }
 
     };
-    // const getMovieforThisShow = async () => {
-    //     const movieData = await AxiosInstance({
-    //         url: '/users/getmoviebyshow',
-    //         method: 'GET',
-    //         params: {
-    //             showId: showsid
-    //         }
-    //     });
-    //     const preMovieid= movieData.data.message;
-    //     const movieRes= preMovieid[0].movie
-    //     setMovieId(movieRes);
-    //     console.log(movieId);
+    const getMovieforThisShow = async () => {
+        const movieData = await AxiosInstance({
+            url: '/users/getmoviebyshow',
+            method: 'GET',
+            params: {
+                showId: showsid
+            }
+        });
+        const preMovieid = movieData.data.message;
+        const movieRes = preMovieid[0].movie
+        setMovieId(movieRes);
+        console.log(movieId);
 
-    // }
+    }
     const selectSeats = (seatNumber) => {
         setSelectedSeats([...selectedSeats, seatNumber]);
         // setseatisSelected(!seatisSelected)
@@ -56,6 +59,82 @@ function TheatreSeats() {
 
 
     }
+    // function loadScript(src) {
+    //     return new Promise((resolve) => {
+    //         const script = document.createElement("script");
+    //         script.src = src;
+    //         script.onload = () => {
+    //             resolve(true);
+    //         };
+    //         script.onerror = () => {
+    //             resolve(false);
+    //         };
+    //         document.body.appendChild(script);
+    //     });
+    // }
+
+    async function displayRazorpay() {
+        const res = await loadScript(
+            "https://checkout.razorpay.com/v1/checkout.js"
+        );
+
+        if (!res) {
+            alert("Razorpay SDK failed to load. Are you online?");
+            return;
+        }
+
+        const result = await AxiosInstance.post("/payments/orders", { showId: showsid, seats: selectedSeats, theatreId: id, movieId: movieId });
+        console.log(result);
+
+        if (!result) {
+            alert("Server error. Are you online?");
+            return;
+        }
+
+        const { amount, id: order_id, currency ,receipt} = result.data;
+
+        const options = {
+            key:`${import.meta.env.VITE_RP_KEY_ID}`,
+            amount: amount.toString(),
+            currency: currency,
+            name: "Tap Tickets Online",
+            description: "Booking Seats",
+            image: null,
+            order_id: order_id,
+            handler: async function (response) {
+                const data = {
+                    orderCreationId: order_id,
+                    razorpayPaymentId: response.razorpay_payment_id,
+                    razorpayOrderId: response.razorpay_order_id,
+                    razorpaySignature: response.razorpay_signature,
+                    receipt,
+                    theatreId:id,
+                    showId:showsid,
+                    movieId: movieId
+                };
+
+                const result = await AxiosInstance.post("/payments/verify", data);
+
+                // alert(result.data.msg);
+                successToast('Booking was completed');
+                getSeats();   
+            },
+            prefill: {
+                name: user.firstName,
+                email: user.email,
+                contact:'9999999999',
+            },
+            // notes: {
+            //     address: "Soumya Dey Corporate Office",
+            // },
+            theme: {
+                color: "#61dafb",
+            },
+        };
+
+        const paymentObject = new window.Razorpay(options);
+        paymentObject.open();
+    };
     function loadScript(src) {
         return new Promise((resolve) => {
             const script = document.createElement("script");
@@ -68,62 +147,6 @@ function TheatreSeats() {
             };
             document.body.appendChild(script);
         });
-    }
-
-    async function displayRazorpay() {
-        const res = await loadScript(
-            "https://checkout.razorpay.com/v1/checkout.js"
-        );
-
-        if (!res) {
-            alert("Razorpay SDK failed to load. Are you online?");
-            return;
-        }
-
-        const result = await AxiosInstance.post("/payments/orders", { showId: showsid, seats: selectedSeats, theatreId: id});
-
-        if (!result) {
-            alert("Server error. Are you online?");
-            return;
-        }
-
-        const { amount, id: order_id, currency } = result.data;
-
-        const options = {
-            key: "rzp_test_r6FiJfddJh76SI", // Enter the Key ID generated from the Dashboard
-            amount: amount.toString(),
-            currency: currency,
-            name: "Soumya Corp.",
-            description: "Test Transaction",
-            image: null,
-            order_id: order_id,
-            handler: async function (response) {
-                const data = {
-                    orderCreationId: order_id,
-                    razorpayPaymentId: response.razorpay_payment_id,
-                    razorpayOrderId: response.razorpay_order_id,
-                    razorpaySignature: response.razorpay_signature,
-                };
-
-                const result = await AxiosInstance.post("/payments/success", data);
-
-                alert(result.data.msg);
-            },
-            prefill: {
-                name: "Soumya Dey",
-                email: "SoumyaDey@example.com",
-                contact: "9999999999",
-            },
-            notes: {
-                address: "Soumya Dey Corporate Office",
-            },
-            theme: {
-                color: "#61dafb",
-            },
-        };
-
-        const paymentObject = new window.Razorpay(options);
-        paymentObject.open();
     }
 
     return (
